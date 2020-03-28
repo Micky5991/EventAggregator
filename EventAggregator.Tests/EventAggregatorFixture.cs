c9f67cc30c8c1762065595df02ddcf52bf78ff42
Francesco Paolocci <micky5991@gmail.com>
2020-03-28 22:43:15 +0100

    Add tests for correct event aggregator calls

--- EventAggregator.Tests/EventAggregatorFixture.cs
+++ EventAggregator.Tests/EventAggregatorFixture.cs
@@ -45,6 +45,15 @@ namespace EventAggregator.Tests
             result.Should().BeAssignableTo<ISubscription>();
         }
 
+        [TestMethod]
+        public void SubscribeToSyncEventWillReturnSubscriptionObject()
+        {
+            var result = _eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.Normal);
+
+            result.Should().NotBeNull();
+            result.Should().BeAssignableTo<ISyncSubscription>();
+        }
+
         [DataTestMethod]
         [DataRow(1)]
         [DataRow(2)]
@@ -56,6 +65,31 @@ namespace EventAggregator.Tests
             for (var i = 0; i < amount; i++)
             {
                 subscriptions.Add(_eventAggregator.Subscribe<TestEvent>(TestCallbackAsync, null, EventPriority.Normal));
+            }
+
+            var cache = _eventAggregator.OrderedSubscriptions;
+
+            cache.Should().NotBeEmpty();
+            cache.Should().HaveCount(1);
+            cache.Should().ContainKey(typeof(TestEvent));
+
+            var typeCache = cache[typeof(TestEvent)];
+
+            typeCache.Should().Contain(subscriptions);
+            typeCache.Count.Should().Be(amount);
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        public void SubscribeToSyncEventWillUpdateCache(int amount)
+        {
+            var subscriptions = new List<ISubscription>();
+
+            for (var i = 0; i < amount; i++)
+            {
+                subscriptions.Add(_eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.Normal));
             }
 
             var cache = _eventAggregator.OrderedSubscriptions;
@@ -82,17 +116,44 @@ namespace EventAggregator.Tests
         }
 
         [TestMethod]
+        public void SyncSubscriptionsShouldBeOrderedInCache()
+        {
+            _eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.Normal);
+            _eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.High);
+            _eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.Low);
+
+            var cache = _eventAggregator.OrderedSubscriptions[typeof(TestEvent)];
+            cache.Should().BeInDescendingOrder(x => x.Priority);
+        }
+
+        [TestMethod]
         public void SubscribingWithNullCallbackThrowsArgumentNullException()
         {
             Action act = () => _eventAggregator.Subscribe<TestEvent>(null, null, EventPriority.Normal);
+            Action actSync = () => _eventAggregator.SubscribeSync<TestEvent>(null, null, EventPriority.Normal);
 
             act.Should().Throw<ArgumentNullException>();
+            actSync.Should().Throw<ArgumentNullException>();
         }
 
         [TestMethod]
         public void UnsubscribeSubscriptionWillRemoveFromLists()
         {
             var subscription = (IInternalSubscription) _eventAggregator.Subscribe<TestEvent>(TestCallbackAsync, null, EventPriority.Normal);
+
+            _eventAggregator.Unsubscribe(subscription);
+
+            var subscriptions = _eventAggregator.Subscriptions;
+            var cache = _eventAggregator.OrderedSubscriptions;
+
+            subscriptions.Count.Should().Be(0);
+            cache.Count.Should().Be(0);
+        }
+
+        [TestMethod]
+        public void UnsubscribeSyncSubscriptionWillRemoveFromLists()
+        {
+            var subscription = (IInternalSubscription) _eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.Normal);
 
             _eventAggregator.Unsubscribe(subscription);
 
@@ -136,30 +197,76 @@ namespace EventAggregator.Tests
         }
 
         [TestMethod]
+        public void UnsubscribeWithUnknownSyncSubscriptionWillDontChangeAnything()
+        {
+            var removedSubscription = (IInternalSubscription) _eventAggregator.SubscribeSync<TestEvent>(TestCallbackSync, null, EventPriority.Normal);
+            _eventAggregator.SubscribeSync<OtherTestEvent>(OtherTestCallbackSync, null, EventPriority.Normal);
+
+            var subscriptions = _eventAggregator.Subscriptions;
+            var cache = _eventAggregator.OrderedSubscriptions;
+
+            cache.Count.Should().Be(2);
+            cache.Should().ContainKeys(typeof(TestEvent), typeof(OtherTestEvent));
+
+            subscriptions.Count.Should().Be(2);
+            subscriptions.Should().ContainKeys(typeof(TestEvent), typeof(OtherTestEvent));
+
+            _eventAggregator.Unsubscribe(removedSubscription);
+
+            cache.Count.Should().Be(1);
+            cache.Should().ContainKeys(typeof(OtherTestEvent));
+
+            subscriptions.Count.Should().Be(1);
+            subscriptions.Should().ContainKeys(typeof(OtherTestEvent));
+
+            _eventAggregator.Unsubscribe(removedSubscription);
+
+            cache.Count.Should().Be(1);
+            cache.Should().ContainKeys(typeof(OtherTestEvent));
+
+            subscriptions.Count.Should().Be(1);
+            subscriptions.Should().ContainKeys(typeof(OtherTestEvent));
+        }
+
+        [TestMethod]
         public async Task TriggerOnEventInterfaceWillTriggerAllEvents()
         {
-            var callAmount = 0;
+            var callAmountAsync = 0;
+            var callAmountSync = 0;
 
-            Task Callback(IEvent eventData)
+            Task CallbackAsync(IEvent eventData)
             {
-                callAmount++;
+                callAmountAsync++;
 
                 return Task.CompletedTask;
             }
 
-            Task CallbackOther(TestEvent eventData)
+            Task CallbackOtherAsync(TestEvent eventData)
             {
-                callAmount++;
+                callAmountAsync++;
 
                 return Task.CompletedTask;
             }
 
-            _eventAggregator.Subscribe<IEvent>(Callback, null, EventPriority.Normal);
-            _eventAggregator.Subscribe<TestEvent>(CallbackOther, null, EventPriority.Normal);
+            void CallbackSync(IEvent eventData)
+            {
+                callAmountSync++;
+            }
+
+            void CallbackOtherSync(TestEvent eventData)
+            {
+                callAmountSync++;
+            }
+
+            _eventAggregator.Subscribe<IEvent>(CallbackAsync, null, EventPriority.Normal);
+            _eventAggregator.Subscribe<TestEvent>(CallbackOtherAsync, null, EventPriority.Normal);
+            _eventAggregator.SubscribeSync<IEvent>(CallbackSync, null, EventPriority.Normal);
+            _eventAggregator.SubscribeSync<TestEvent>(CallbackOtherSync, null, EventPriority.Normal);
 
             await _eventAggregator.PublishAsync(new TestEvent());
 
-            callAmount.Should().Be(2);
+            callAmountAsync.Should().Be(2);
+            callAmountSync.Should().Be(2);
         }
 
         [TestMethod]
@@ -177,6 +284,75 @@ namespace EventAggregator.Tests
             await act.Should().NotThrowAsync();
         }
 
+        [TestMethod]
+        public void ThrowingExceptionInSyncHandlerDoesNotThrowException()
+        {
+            void Callback(IEvent eventData)
+            {
+                throw new TestException();
+            }
+
+            _eventAggregator.SubscribeSync<IEvent>(Callback, null, EventPriority.Normal);
+
+            Action act = () => _eventAggregator.PublishSync(new TestEvent());
+
+            act.Should().NotThrow();
+        }
+
+        [TestMethod]
+        public async Task AsyncCallWillCallSyncAndAsyncSubscriptions()
+        {
+            var callAmountSync = 0;
+            var callAmountAsync = 0;
+
+            void Callback(IEvent eventData)
+            {
+                callAmountSync++;
+            }
+
+            Task CallbackAsync(IEvent eventData)
+            {
+                callAmountAsync++;
+
+                return Task.CompletedTask;
+            }
+
+            _eventAggregator.SubscribeSync<TestEvent>(Callback, null, EventPriority.Normal);
+            _eventAggregator.Subscribe<TestEvent>(CallbackAsync, null, EventPriority.Normal);
+
+            await _eventAggregator.PublishAsync(new TestEvent());
+
+            callAmountAsync.Should().Be(1);
+            callAmountSync.Should().Be(1);
+        }
+
+        [TestMethod]
+        public void SyncCallWillCallOnlySyncSubscriptions()
+        {
+            var callAmountSync = 0;
+            var callAmountAsync = 0;
+
+            void Callback(IEvent eventData)
+            {
+                callAmountSync++;
+            }
+
+            Task CallbackAsync(IEvent eventData)
+            {
+                callAmountAsync++;
+
+                return Task.CompletedTask;
+            }
+
+            _eventAggregator.SubscribeSync<TestEvent>(Callback, null, EventPriority.Normal);
+            _eventAggregator.Subscribe<TestEvent>(CallbackAsync, null, EventPriority.Normal);
+
+            _eventAggregator.PublishSync(new TestEvent());
+
+            callAmountAsync.Should().Be(0);
+            callAmountSync.Should().Be(1);
+        }
+
         private Task TestCallbackAsync(TestEvent testEvent)
         {
             return Task.CompletedTask;
@@ -185,6 +361,16 @@ namespace EventAggregator.Tests
         private Task OtherTestCallbackAsync(OtherTestEvent testEvent)
         {
             return Task.CompletedTask;
+        }
+
+        private void TestCallbackSync(TestEvent testEvent)
+        {
+            // Do nothing
+        }
+
+        private void OtherTestCallbackSync(OtherTestEvent testEvent)
+        {
+            // Do Nothing
         }
     }
 }
