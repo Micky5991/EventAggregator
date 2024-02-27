96006c057cef86f3ea886a5f5538054e68e6998d
Francesco Paolocci <micky5991@gmail.com>
2024-02-27 22:08:27 +0100

    Add validation to subscription properties

--- EventAggregator/Elements/SubscriptionOptions.cs
+++ EventAggregator/Elements/SubscriptionOptions.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Runtime.CompilerServices;
+using CommunityToolkit.Diagnostics;
 using Micky5991.EventAggregator.Interfaces;
 
 namespace Micky5991.EventAggregator.Elements;
 
 public class SubscriptionOptions
 {
+    private EventPriority _eventPriority = EventPriority.Normal;
+    private ThreadTarget _threadTarget = ThreadTarget.PublisherThread;
+
     /// <summary>
     /// Gets a value indicating when the eventtype implements <see cref="ICancellableEvent"/> and a lower priority handler cancels the event, this handler won't be invoked.
     /// </summary>
@@ -12,10 +18,44 @@ public class SubscriptionOptions
     /// <summary>
     /// Gets the priority that orders handler execution from low to high.
     /// </summary>
-    public EventPriority EventPriority { get; set; } = EventPriority.Normal;
+    public EventPriority EventPriority
+    {
+        get => _eventPriority;
+        set
+        {
+            ValidateEventPriority(value);
+
+            _eventPriority = value;
+        }
+    }
 
     /// <summary>
     /// Gets the target thread where this event should be executed in.
     /// </summary>
-    public ThreadTarget ThreadTarget { get; set; } = ThreadTarget.PublisherThread;
+    public ThreadTarget ThreadTarget
+    {
+        get => _threadTarget;
+        set
+        {
+            ValidateThreadTarget(value);
+
+            _threadTarget = value;
+        }
+    }
+
+    private static void ValidateEventPriority(EventPriority priority, [CallerArgumentExpression(nameof(priority))] string? argument = null)
+    {
+        if (Enum.IsDefined(typeof(EventPriority), priority) == false)
+        {
+            throw new ArgumentOutOfRangeException(argument, priority, $"{priority} is not defined in {typeof(EventPriority)}");
+        }
+    }
+
+    private static void ValidateThreadTarget(ThreadTarget threadTarget, [CallerArgumentExpression(nameof(threadTarget))] string? argument = null)
+    {
+        if (Enum.IsDefined(typeof(ThreadTarget), threadTarget) == false)
+        {
+            throw new ArgumentOutOfRangeException(argument, threadTarget, $"{threadTarget} is not defined in {typeof(EventPriority)}");
+        }
+    }
 }
